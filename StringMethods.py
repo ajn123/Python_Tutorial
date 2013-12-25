df7793d25fa51671286a1bce32d580210cb7ed3a
AJ Norton <aj123@vt.edu>
2013-12-25 03:45:45 -0500

    added input example

--- StringMethods.py
+++ StringMethods.py
@@ -9,19 +9,31 @@ multiline comment"""
 #This is a single line comment
 
 
+#This class allows for junit testing
+#Just use assert Equals
 class MyTest(unittest.TestCase):
 
-    def test3(self):
-    	print "testing string"
-        self.assertEqual("YOLO"[0:2], "YO")
-
+	#Tests string length method
     def testLength(self):
-    	self.assertEqual(1,len("A"));
+    	string = "practice String"
+    	#Tests the length of a string
+        self.assertEqual(15, len(string))
+
+    def testUpper(self):
+    	string = "practice String"
+    	self.assertEqual("PRACTICE STRING",string.upper());
 
 
-    def testStringMethod(self):
-    	self.assertEqual("3.14",(str(3.14)))
+    def testLower(self):
+    	string = "Practice String"
+    	self.assertEqual("practice string",string.lower());
 
+
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
 
 
 unittest.main()
