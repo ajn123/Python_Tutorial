d7e480f3cb99d7ed571b207a3d7159740c565db5
AJ Norton <aj123@vt.edu>
2013-12-25 14:10:42 -0500

    added class example

---
+++ classes.py
@@ -0,0 +1,16 @@
+
+
+
+class myClass():
+	def method1(self):
+		print "this is a class method"
+		
+
+def main():
+	c = myClass()
+	c.method1()
+
+
+if __name__ == '__main__':
+	main()
+		
--- loops.py
+++ loops.py
@@ -7,8 +7,14 @@ def main():
 		print x
 		x = x+1
 
-	#this is a for loop
+	#this is a for loop, it increments by one automaticaaly
+	#it goes from 5 to 10 exclusive so  5,6,7,8,9 will be printed NOT 10
 	for x in range(5,10):
+		print x
+
+	#The 2 represents how big the incrementer is, default is 1, this example is 2
+	#this will print out 5 7 9
+	for x in range(5,10,2):
 		print x
 
 
