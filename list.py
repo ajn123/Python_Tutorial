2a75c8042356c7b31c021d85494288ef28ce3090
AJ Norton <aj123@vt.edu>
2013-12-27 23:35:46 -0500

    gets stuff done

--- list.py
+++ list.py
@@ -1,6 +1,23 @@
 import unittest
 
 
+"""
+A List represents the most versatile type of data structure in Python. It can contain items of different 
+types and it has no rule against unicity. List indices start from zero, the elements can be sliced, 
+concatenated, and so on. Lists also have a lot of similarities with strings, supporting the same kind 
+of operations but unlike strings, lists are mutable.
+
+USE LISTS WHEN:
+
+When the data needs to be ordered.
+When you need a mixed collection of data all in one place.
+Data can contain duplicates
+When your data requires the ability to be changed or extended. Remember, lists are mutable.
+"""
+
+
+
+
 
 class listTest(unittest.TestCase):
 
@@ -15,6 +32,19 @@ class listTest(unittest.TestCase):
 		list = [1,10,3]
 		list.sort()
 		self.assertEqual(list,[1,3,10])
+
+
+	#Retrieve data from list using bracket notation
+	def testRetrieve(self):
+		list = [1,10,3]
+		self.assertEqual(list[0],1)
+		self.assertEqual(list[1],10)
+
+	def testModify(self):
+		list = [1,2,3]
+		list[0] = 100
+		self.assertEqual(list[0],100)
+
 
 
 
