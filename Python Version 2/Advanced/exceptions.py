7985ae9bcb0ef02a346144b0066e0f7a13fc24a9
AJ Norton <aj123@vt.edu>
2014-01-13 05:39:35 -0500

    added exception example

--- Python Version 2/Advanced/exceptions.py
+++ Python Version 2/Advanced/exceptions.py
@@ -1,7 +1,30 @@
-while True:
-	#Similar to Try and catch, tries an error and catches it 
+"""
+An exception is what occurs when you have a run time error in your 
+program.
+"""
+
+
+def main():
+	while True:
+		#Similar to Try and catch, tries an error and catches it 
+		try:
+			x = int(raw_input("Please enter a number: "))
+			break
+		except ValueError:
+	         print "Oops!  That was no valid number.  Try again..."
+
+
+
+	#a lookup error in whon an a incorrect lookup happens in a dictionary,
+	dict = {"string": "a word representation"}
+
 	try:
-		x = int(raw_input("Please enter a number: "))
-		break
-	except ValueError:
-         print "Oops!  That was no valid number.  Try again..."
+		dict["ERROR"]
+	except LookupError:
+		print LookupError
+
+
+
+
+if __name__ == '__main__':
+	main()
