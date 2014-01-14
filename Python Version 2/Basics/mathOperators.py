f74e37f5de09c4fbe8e7a7e68c943ebb74d17669
AJ Norton <aj123@vt.edu>
2014-01-13 20:37:09 -0500

    finished math operators

--- Python Version 2/Basics/mathOperators.py
+++ Python Version 2/Basics/mathOperators.py
@@ -32,9 +32,47 @@ def main():
 	print "{0} % {1}  =  {2}".format(a,b,sum)	
 
 	
+	print "sum is now =  {2}".format(a,b,sum)	
+
+
+
 	#increments the value stored in sum by 5
 	sum += 5
-	print "{0} / {1}  =  {2}".format(a,b,sum)	
+	print "sum is now =  {2}".format(a,b,sum)	
+
+
+
+	sum -= 5
+	print "sum is now =  {2}".format(a,b,sum)	
+
+
+	sum *= 5
+	print "sum is now =  {2}".format(a,b,sum)
+
+	sum /= 5
+	print "sum is now =  {2}".format(a,b,sum)	
+
+	sum %= 5
+	print "sum is now =  {2}".format(a,b,sum)	
+
+
+	a = 10
+	b = 2
+	sum = pow(a,b)
+	print "{0} to the power of {1}  =  {2}".format(a,b,sum)	
+
+
+	a = 10
+	b = 2
+	#divmod gives the result of division and modulus
+	sum = divmod(a,b)
+	print "{0} divided by {1}  = {2}  and its mod is  {3}".format(a,b,sum[0],sum[1])	
+
+
+
+
+
+
 
 
 
