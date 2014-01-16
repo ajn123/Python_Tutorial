5ca4e336045ac47908774b1dd21dcd87786b1d26
AJ Norton <aj123@vt.edu>
2014-01-15 19:48:23 -0500

    added pass file

---
+++ Python Version 2/Basics/pass.py
@@ -0,0 +1,32 @@
+
+
+"""
+The pass statement does nothing. It can be used when a statement is required syntactically but the program requires no action. For example:
+
+Another place pass can be used is as a place-holder for a function or conditional body when you are working on new code,
+allowing you to keep thinking at a more abstract level. The pass is silently ignored:
+
+
+
+
+Pass is mainly used when you make a function or a loop and want to fill it in later but you still want to be able to run your program.
+"""
+
+
+
+
+def main():
+	while True:
+		pass  # Busy-wait for keyboard interrupt (Ctrl+C)
+
+
+
+def initlog(*args):
+	pass   # Remember to implement this!
+
+
+
+
+
+if __name__ == '__main__':
+	main()
