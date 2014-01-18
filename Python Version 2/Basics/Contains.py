ad980551f3f4bb24770aee40d6fd39cd25baf2c0
AJ Norton <aj123@vt.edu>
2014-01-18 00:05:06 -0500

    contains

--- Python Version 2/Basics/Contains.py
+++ Python Version 2/Basics/Contains.py
@@ -10,11 +10,10 @@ def main():
 	The "in" keyword is an operator you can use on a set to see in that 
 	element exists in the set.
 
+	This is useful if you want to check if a certain element is in a set and dont
+	want to loop through it and check with a while loop.
 	"""
 	print 2 in a #True
-
-
-
 
 
 	print 5 in range(4,11) #True
@@ -31,6 +30,13 @@ def main():
 	There also is a NOT keyword to see if an inverse of a set is true.
 	"""
 	print 10 not in a #True, 10 is not in the list of a.
+
+
+
+	#outputs an array of true or false checking if  144 is the value or not
+	cats = [x**2  is 144 for x in range(10,15)]
+
+	print cats  # [False, False, True, False, False]
 
 
 
