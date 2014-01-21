2b05599477af3ee803a6aff178fff0fe793ff390
AJ Norton <aj123@vt.edu>
2014-01-21 01:44:03 -0500

    set update

--- Python Version 2/Advanced/set Methods.py
+++ Python Version 2/Advanced/set Methods.py
@@ -8,9 +8,11 @@ def main():
 	a ={3,4,5,6,1}
 
 
+	b = set([1,2,3])  #Another way to make a set.
+
 
 	print a & list  #intersection operation (elemients in both sets)
-	print a  | list #union operation (elements combinted into one set)
+	print a | list #union operation (elements combinted into one set)
 	print a - list #difference operation (removes all the identical elements from the the second parameter set.
 	print a ^ list #symmetric difference (elements that are in the first set and the second, but not in both)
 
