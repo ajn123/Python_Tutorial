bab1ef1c805e6123b905d34293744f50713fc2dc
AJ Norton <ajn123@vt.edu>
2014-04-16 00:43:32 -0400

    added lamda expressions

--- Python Version 2/Advanced/lamdaExpressions.py
+++ Python Version 2/Advanced/lamdaExpressions.py
@@ -1,19 +1,34 @@
-
-
 """
 
-Lamda expressions have special rules:
-1)  The body of a Lamda expression can contain only a single line.
+lambda expressions have special rules:
+1)  The body of a lambda expression can contain only a single line.
 2)  The body can only contain an expression, no statements or other elements
 3)  The result of the expression is automatically returned.
 
-The form of a lamda expression is:
+The form of a lambda expression is:
 	lamda arg_list: expression
+
+A lambda is essentially an anonymous function, a function without a name
 
 """
 
 
-
+#I am now assigning this lambda to add so now add is pointing to this lambda function and can call it.
 add = lambda num1, num2: num2  + num1
 
+
+#calling lambda function through add.
 print add(2,3) #prints 5.
+
+
+"""
+Another way to use lambda functions is with the map() function, it takes a function and an iterable 
+and applies that function to all the iterables.
+
+I defined a lambda expression to square everything and map() applied that function to everything in the
+list.
+"""
+result = map(lambda x: x**2,[2,3,4,56])
+
+print list(result)
+
