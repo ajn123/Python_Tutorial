85880a4ef6c84714c43d5c59f88575dce97b4930
AJ Norton <ajn123@vt.edu>
2014-04-17 10:24:11 -0400

    added InputExample.py

--- Python Version 2/Basics/InputExample.py
+++ Python Version 2/Basics/InputExample.py
@@ -8,6 +8,7 @@ def main():
 	quest = raw_input("What is your quest?")
 	color = raw_input("What is your favorite color?")
 
+	#the '\' lets you function calls on multiple lines.
 	print "Ah, so your name is %s, your quest is %s, " \
 	"and your favorite color is %s." % (name, quest, color)
 
